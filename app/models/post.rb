ff35fdec78e40fffffdb0be2b4e84600c76c40ca
billyott <willrichott@gmail.com>
2020-10-15 13:29:38 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,12 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true
+    # validates :category, :inclusion=> { :in => @allowed_cats }
+    # validates :content, length: {minimum: 100}
+    validates :content, length: { minimum: 100 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+
+
+    # def allowed_cats
+    #     @allowed_cats = ["Fiction","Non-Fiction"]
+    # end
 end
